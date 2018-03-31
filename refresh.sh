95017414b6341cf9d716d8150b2df41e4386eedf
aleqx <8780617+aleqx@users.noreply.github.com>
2018-03-31 18:28:05 +0100

    bug: stderr output from 'docker logs'

--- refresh.sh
+++ refresh.sh
@@ -25,20 +25,20 @@ logkeep=1000
 # EDIT: it seems that `date -d STR` does convert to the current timezone as long as STR includes
 # the timezone at the end, or if it's formatted as {date}T{time}Z, which apparently is assumed GMT
 
-lastchall=`docker logs zen-secnodetracker|grep 'Challenge result'|tail -n 1`
+lastchall=`docker logs zen-secnodetracker 2>/dev/null|grep 'Challenge result'|tail -n 1`
 [[ $lastchall ]] || exit  # don't restart if a challenge hasn't happened (don't know if imminent)
 
 lastchall=${lastchall/ -- *}
 lastchall=`date -d"$lastchall" +%s`
 agochall=$((`date +%s`-lastchall))
 
-echo "`date +'%F %T'` Last challenge was $((agochall/3600))h $(((agochall%3600)/60))m ago."
-
-[[ $agochall -gt $((maxagochall*3600)) ]] && exit  # don't restart if last challenge too long ago (likely imminent)
+echo -n "`date +'%F %T'` Last challenge was $((agochall/3600))h $(((agochall%3600)/60))m ago. "
+[[ $agochall -gt $((maxagochall*3600)) ]] && echo "Skipping ..." && exit  # don't restart if last challenge too long ago (likely imminent)
+echo
 
 # wait for a successful stats to be received from the tracker
 for ((i=600;i>0;i-=poll)); do
-    laststats=`docker logs --tail=10 zen-secnodetracker|grep 'Stats received by '|tail -n 1`
+    laststats=`docker logs --tail=50 zen-secnodetracker 2>/dev/null|grep 'Stats received by '|tail -n 1`
     [[ ! $laststats ]] && sleep $poll && continue
     laststats=${laststats/ -- *}
     #laststats=`date -d"$laststats" +%s`
