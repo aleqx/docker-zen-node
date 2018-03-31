163584b3234a039646d9a576d0288cc542366e4b
aleqx <8780617+aleqx@users.noreply.github.com>
2018-03-31 17:41:35 +0100

    added web install

--- refresh.sh
+++ refresh.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+#
+# To install from web, do:
+#   curl https://raw.githubusercontent.com/aleqx/docker-zen-node/master/refresh.sh|INSTALL=1 bash
+#
 
-poll=30
+poll=30         # seconds, poll secure node logs every T seconds for the tracker reply
 maxagochall=20  # hours, if last challenge was more than this many hours ago then quit (a challenge may be imminent)
-url
+url="https://raw.githubusercontent.com/aleqx/docker-zen-node/master/refresh.sh"
+
+[[ $INSTALL = 1 ]] && {
+    echo "Installing in cron.d ..."
+    curl -s -o /root/zen-refresh.sh "$url"
+    echo "35 */6 * * *  root  bash /root/zen-refresh.sh >/dev/null" > /etc/cron.d/zen-refresh
+    exit
+}
 
 #export TZ=`date +'%Z %z'`  # for converting times to current timezone
 # EDIT: it seems that `date -d STR` does convert to the current timezone as long as STR includes
@@ -21,17 +32,17 @@ echo "Last challenge was $((agochall/3600))h $(((agochall%3600)/60))m ago."
 
 # wait for a successful stats to be received from the tracker
 for ((i=600;i>0;i-=poll)); do
-        laststats=`docker logs --tail=10 zen-secnodetracker|grep 'Stats received by '|tail -n 1`
-        [[ ! $laststats ]] && sleep $poll && continue
-        laststats=${laststats/ -- *}
-        #laststats=`date -d"$laststats" +%s`
-        agostats=$((`date +%s`-`date -d"$laststats" +%s`))
-        if [[ $agostats -gt 5 && $agostats -lt 75 ]]; then
-                echo "Last stats was at $laststats ($agostats seconds ago). Restarting zen node ..."
-                echo systemctl restart zen-node
-                break
-        else
-                echo "Last stats was at $laststats ($agostats seconds ago). Waiting ..."
-        fi
-        sleep $poll
+    laststats=`docker logs --tail=10 zen-secnodetracker|grep 'Stats received by '|tail -n 1`
+    [[ ! $laststats ]] && sleep $poll && continue
+    laststats=${laststats/ -- *}
+    #laststats=`date -d"$laststats" +%s`
+    agostats=$((`date +%s`-`date -d"$laststats" +%s`))
+    if [[ $agostats -gt 5 && $agostats -lt 75 ]]; then
+        echo "Last stats was at $laststats ($agostats seconds ago). Restarting zen node ..."
+        echo systemctl restart zen-node
+        break
+    else
+        echo "Last stats was at $laststats ($agostats seconds ago). Waiting ..."
+    fi
+    sleep $poll
 done
